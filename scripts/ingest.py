5dcb8c1921a9403affd7ea2dbfbcc473d642002c
prashant00797 <prashantnath6307@gmail.com>
2026-04-23 18:51:06 +0530

    Phase 1 - creating documents , splitting the docs and chunking it, creation of vector db and storing in picone vector db

---
+++ scripts/ingest.py
@@ -0,0 +1,35 @@
+import os
+from pinecone import Pinecone
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_pinecone import PineconeVectorStore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from app.core.config import Settings
+
+config = Settings() # type: ignore
+os.environ["PINECONE_API_KEY"] = config.PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = config.OPENAI_API_KEY
+
+# initializing imports for indexing
+document_path = "./knowledge_base/Prashant_kb.pdf"
+loader = PyPDFLoader(document_path)
+splitter = RecursiveCharacterTextSplitter(chunk_size=500,chunk_overlap=100)
+
+
+# pincone intialization
+pc = Pinecone(pinecone_api_key=config.PINECONE_API_KEY)
+index = pc.Index(config.PINECONE_INDEX_NAME) # type: ignore
+index.delete(delete_all=True)
+
+# indexing
+if(os.path.exists(document_path)):
+   docs = loader.load()
+   chunks = splitter.split_documents(docs)
+   embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+   vector_store = PineconeVectorStore.from_documents(
+       documents=chunks,
+       embedding=embeddings,
+       index_name=config.PINECONE_INDEX_NAME
+   )
+else:
+   print("Document not Found",document_path)
